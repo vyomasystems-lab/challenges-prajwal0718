f14269d03b50e01e1cf20fb0c19097b8ee415bf6
Prajwal D Nayak <rdpraju7@gmail.com>
2022-08-01 15:47:06 +0000

    updda

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,34 @@
 
 import cocotb
 from cocotb.triggers import Timer
-
+print('Prajwal D Nayak')
 @cocotb.test()
 async def test_mux(dut):
-    """Test for mux2"""
+    dut.sel.value =5'b00000;
+    
+    dut.sel.value = 5'b00001;
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.sel.value = 5'b00010;
+    
+    dut.sel.value = 5'b10010;
+   
+     dut.sel.value = 5'b10011;
+    
+     dut.sel.value = 5'b10110;
+   
+
+    assert dut.out.value == 5'b11111 , Mux result takes default value;
+
+
+    cocotb.log.info( dut.sel.value =5'b00000;
+    await Timer(2,units='ns')
+    dut.sel.value = 5'b00001;
+    await Timer(2,units='ns')
+    dut.sel.value = 5'b00010;
+    await Timer(2,units='ns')
+    dut.sel.value = 5'b10010;
+    await Timer(2,units='ns')
+     dut.sel.value = 5'b10011;
+    await Timer(2,units='ns')
+     dut.sel.value = 5'b10110;
+    await Timer(2,units='ns'))
